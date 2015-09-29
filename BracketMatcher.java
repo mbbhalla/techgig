68120188746cd4a0ea1eb68b3e56ccebdb56c520
MABH <manujbhalla@outlook.com>
2015-09-29 22:22:17 +0530

    Problem: bracket balance matcher

--- BracketMatcher.java
+++ BracketMatcher.java
@@ -7,22 +7,21 @@ import java.util.Set;
 import java.util.Stack;
 
 public class BracketMatcher {
+
 	@SuppressWarnings("serial")
-	public static void main(String[] args) {
-		try (Scanner scanner = new Scanner(System.in);) {
-			System.out.println("Tell? : ");
-			String input = scanner.nextLine();
-			
-			Stack<String> stack = new Stack<>();
-			
-			Set<String> open = new HashSet<>(Arrays.asList("(", "{", "["));
-			Set<String> close = new HashSet<>(Arrays.asList(")", "}", "]"));
-			Map<String, String> map = new HashMap<String, String>() {{ 
-				put(")", "(");
-				put("}", "{");
-				put("]", "[");
-			}};
-			Arrays.asList(input.split(""))
+	private static boolean matcher(final String arg) {
+		Stack<String> stack = new Stack<>();
+		
+		Set<String> open = new HashSet<>(Arrays.asList("(", "{", "["));
+		Set<String> close = new HashSet<>(Arrays.asList(")", "}", "]"));
+		Map<String, String> map = new HashMap<String, String>() {{ 
+			put(")", "(");
+			put("}", "{");
+			put("]", "[");
+		}};
+		
+		try {
+			Arrays.asList(arg.split(""))
 				.stream()
 				.forEach(s -> {
 					if(open.contains(s)) {
@@ -30,13 +29,22 @@ public class BracketMatcher {
 					} else if(close.contains(s)) {
 						String popped = stack.pop();
 						if(!map.get(s).equals(popped)) {
-							System.out.println("error");
-							return;
+							throw new RuntimeException();
 						}
 					}
 				});
-			
-			System.out.println("matched");
+			return true;
+		} catch(Exception e) {
+			return false;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in);) {
+			System.out.println("Tell? : ");
+			String input = scanner.nextLine();
+			System.out.println(matcher(input) ? "balanced" : "unbalanced");
 		}
 	}
 }
